59260a02726afb6a3b0c9a22b25230c857251c17
syed shaida <syedshaida295@gmail.com>
2022-03-17 14:48:18 +0530

    Refactor : changed class to className

--- src/pages/landing-page/LandingPage.js
+++ src/pages/landing-page/LandingPage.js
@@ -4,30 +4,30 @@ import productImg from "../../assets/images/product-image.svg"
 
 const LandingPage = () => {
     return <div>
-         <header class="header-nav flex">
-        <ul class="navigation navbar-left flex">
-            <li><a href="/index.html" class="nav-pill active-link">Home</a></li>
-            <li><a href="/Product-listing-page/products.html" class="nav-pill">Products</a></li>
+         <header className="header-nav flex">
+        <ul className="navigation navbar-left flex">
+            <li><a href="/index.html" className="nav-pill active-link">Home</a></li>
+            <li><a href="/Product-listing-page/products.html" className="nav-pill">Products</a></li>
         </ul>
 
-        <input class="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar" type="text" placeholder="Search" />
 
-        <ul class="navigation navbar-right flex">
+        <ul className="navigation navbar-right flex">
             <li>
-                <div class="badge-on-cart wishlist-icon">
-                    <a href="/Wishlist/wishlist.html" class="nav-pill"><i class="material-icons"> favorite_border</i></a>
-                    <p class="icon-badge aligned-icon">5</p>
+                <div className="badge-on-cart wishlist-icon">
+                    <a href="/Wishlist/wishlist.html" className="nav-pill"><i className="material-icons"> favorite_border</i></a>
+                    <p className="icon-badge aligned-icon">5</p>
                 </div>   
             </li>
 
             <li>
-                <a href="/Authentication/login.html" class="nav-pill account-icon"><i class="material-icons"> account_circle</i></a>
+                <a href="/Authentication/login.html" className="nav-pill account-icon"><i class="material-icons"> account_circle</i></a>
             </li>
 
             <li>
-                <div class="badge-on-cart cart-icon">
-                    <a href="/Cart/cart.html" class="nav-pill"><i class="material-icons"> add_shopping_cart</i></a>
-                    <p class="icon-badge aligned-icon">1</p>
+                <div className="badge-on-cart cart-icon">
+                    <a href="/Cart/cart.html" className="nav-pill"><i className="material-icons"> add_shopping_cart</i></a>
+                    <p className="icon-badge aligned-icon">1</p>
                 </div>
                 
             </li>
@@ -35,57 +35,57 @@ const LandingPage = () => {
         </ul>
     </header>
 
-    <main class="home-main">
-        <div class="welcome-text">
-            <p class="line-height">
+    <main className="home-main">
+        <div className="welcome-text">
+            <p className="line-height">
                 Hey there user! Welcome to 
-                <span class="highlighted-text"> Fashion Hub</span> the complete
+                <span className="highlighted-text"> Fashion Hub</span> the complete
                 clothing store. Explore all kinds of clothing stuff
-                <a class="nav-pill" href="">here.</a>
+                <a className="nav-pill" href="">here.</a>
             </p>
-            <p class="line-height">
+            <p className="line-height">
                 Don't stop there! fill your cart to its fullest because that is the
                 thing you can do for free. Don't miss out on free things.
             </p>
-            <p class="line-height">
+            <p className="line-height">
                 Don't stop there either . Your wishlist is feeling lonely. Give it
                 some company by wishlisting some products.
             </p>
-            <p class="line-height">
+            <p className="line-height">
                 Try to avail all our offers and enjoy delivery at your door steps.
             </p>
-            <p class="line-height">
+            <p className="line-height">
                 You are ready to go!
-                <span class="highlighted-text">let's bag it.</span>
+                <span className="highlighted-text">let's bag it.</span>
             </p>
         </div>
 
-        <img class="landing-img responsive-img" src= {landingImg} alt="landing-pic" />
+        <img className="landing-img responsive-img" src= {landingImg} alt="landing-pic" />
 
     </main>
 
-    <footer class="suggestion-container flex">
-        <div class="product-suggestion flex">
-            <div class="product-image">
-                <img class="product-suggestion-image" src={productImg} alt="" />
+    <footer className="suggestion-container flex">
+        <div className="product-suggestion flex">
+            <div className="product-image">
+                <img className="product-suggestion-image" src={productImg} alt="" />
             </div>
 
-            <div class="suggestion-text-container">
-                <p class="suggestion-info line-height">Trending</p>
-                <p class="suggestion-text">Summer collection</p>
-                <p class="line-height">Explore wide range of trending stuff.</p>
+            <div className="suggestion-text-container">
+                <p className="suggestion-info line-height">Trending</p>
+                <p className="suggestion-text">Summer collection</p>
+                <p className="line-height">Explore wide range of trending stuff.</p>
             </div>
         </div>
 
-        <div class="product-suggestion flex">
-            <div class="product-image">
-                <img class="product-suggestion-image" src={productImg} alt="" />
+        <div className="product-suggestion flex">
+            <div className="product-image">
+                <img className="product-suggestion-image" src={productImg} alt="" />
             </div>
 
-            <div class="suggestion-text-container">
-                <p class="suggestion-info line-height">New Arrivals</p>
-                <p class="suggestion-text">Winter collection</p>
-                <p class="line-height">Explore wide range of winter stuff here</p>
+            <div className="suggestion-text-container">
+                <p className="suggestion-info line-height">New Arrivals</p>
+                <p className="suggestion-text">Winter collection</p>
+                <p className="line-height">Explore wide range of winter stuff here</p>
             </div>
         </div>
     </footer>
