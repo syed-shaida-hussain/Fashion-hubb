35ebeae09521a4a9ccec79bf05931aa2b8767213
syed shaida <syedshaida295@gmail.com>
2022-04-04 22:22:02 +0530

    Added product provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { ProductProvider } from "./contexts/product-context/product-context";
+// import {FilterProvider} from "./contexts/filter-context/filter-context"
+import {BrowserRouter} from "react-router-dom"
+
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+  <BrowserRouter>
+  <ProductProvider>
     <App />
+  </ProductProvider>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import {ProductListing} from "../pages/product-listing/ProductListing"
+import {Cart} from "../pages/cart/Cart"
 
 export {ProductListing}
+export {Cart}
