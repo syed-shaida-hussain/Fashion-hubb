75409360989698c3346b2323196cf1c94c132ca9
syed shaida <syedshaida295@gmail.com>
2022-04-10 10:56:25 +0530

    Added context for wishlist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Wishlist } from "./pages/wishlist/Wishlist";
+import { Routes , Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path = "/wishlist" element = {<Wishlist />} />
+      </Routes>
     </div>
   );
 }
