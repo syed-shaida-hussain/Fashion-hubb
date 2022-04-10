6efe986791480d94f3845a789515c53ce03dcc79
syed shaida <syedshaida295@gmail.com>
2022-04-10 11:07:26 +0530

    Added quantity to product db.

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,122 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name: "Men premium trendy Jacket",
+    originalPrice: "2000",
+    discountedPrice : "1500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "1",
+    quantity : 1 
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name: "Men premium sports TShirt",
+    originalPrice: "1500",
+    discountedPrice : "1000",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "1",
+    quantity : 1 
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name: "Men winter cool Jacket",
+    originalPrice: "3000",
+    discountedPrice : "2500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "2",
+    quantity : 1 
   },
+  {
+    _id: uuid(),
+    name: "Women premium silk Saree",
+    originalPrice: "3000",
+    discountedPrice : "2800",
+    categoryMen: false,
+    categoryWomen: true,
+    rating : "2",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Men stylish casual shoes",
+    originalPrice: "1000",
+    discountedPrice : "700",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "3",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Women premium stylish shoes",
+    originalPrice: "2000",
+    discountedPrice : "1500",
+    categoryMen: false,
+    categoryWomen: true,
+    rating : "3",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Men formal quality Jacket",
+    originalPrice: "4000",
+    discountedPrice : "3500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "4",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Men sports durable shoes",
+    originalPrice: "1000",
+    discountedPrice : "500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "4",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Women premium party shoes",
+    originalPrice: "1000",
+    discountedPrice : "500",
+    categoryMen: false,
+    categoryWomen: true,
+    rating : "4",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Men formal premium pants",
+    originalPrice: "3000",
+    discountedPrice : "2500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "5",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: " women casual party Jacket",
+    originalPrice: "3000",
+    discountedPrice : "2000",
+    categoryMen: false,
+    categoryWomen: true,
+    rating : "5",
+    quantity : 1 
+  },
+  {
+    _id: uuid(),
+    name: "Men round neck Tshirt",
+    originalPrice: "700",
+    discountedPrice : "500",
+    categoryMen: true,
+    categoryWomen: false,
+    rating : "5",
+    quantity : 1 
+  }
 ];
