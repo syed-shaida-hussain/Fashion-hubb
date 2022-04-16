1a2c14d98da5ed3ab79e48ef3734b72df1c01b36
syed shaida <syedshaida295@gmail.com>
2022-04-16 11:11:24 +0530

    feat : wishlist func.

--- src/pages/wishlist/Wishlist.js
+++ src/pages/wishlist/Wishlist.js
@@ -1,6 +1,7 @@
 import "./Wishlist.css"
 import productImg from "../../assets/images/product-image.svg"
 import { useWishlist } from "../../contexts/wishlist-context/wishlist-context"
+<<<<<<< Updated upstream
 
 const Wishlist = () => {
     const {wishCount , wishItems , setWishItems , setWishCount} = useWishlist()
@@ -45,11 +46,38 @@ const Wishlist = () => {
         </ul>
     </header>
 
+=======
+import { useCart } from "../../contexts/cart-context/cart-context";
+import { Header } from "../../components/Header";
+
+const Wishlist = () => {
+    const {wishCount , wishItems , setWishItems , setWishCount} = useWishlist()
+    const {cartItems , setCartItems , setCartCount} = useCart()
+
+    function moveToCartHandler(product){
+        if(cartItems.find(item => item._id === product._id)) {
+            setCartItems(previousCartItems => previousCartItems.reduce((accum, currentItem) =>  currentItem._id === product._id ? [{...currentItem, quantity: currentItem.quantity + 1 }, ...accum] : [...accum, currentItem],[]));
+            setCartCount(prev => prev + 1)
+            setWishCount(prev => prev - 1 )
+            setWishItems(wishItems.filter(item => item._id !== product._id))
+        } else {
+            setCartItems([...cartItems , product])
+            setCartCount(prev => prev + product.quantity)
+            setWishItems(wishItems.filter(item => item._id !== product._id))
+            setWishCount(prev => prev - 1)
+        }
+    }
+    
+    return <div>
+    <Header />
+        
+>>>>>>> Stashed changes
     <h1 class="heading-text center-text">My Wishlist({wishCount})</h1>
 
     <hr />
 
     <main class="wishlist-main-container">
+<<<<<<< Updated upstream
         {wishItems.map (product => (<section class="wishlist-product-container">
         <img class="product-image" src={productImg} alt="product-image" />
     
@@ -63,6 +91,22 @@ const Wishlist = () => {
         </div>
         <button class="add-to-cart-btn" onClick = { () => moveToCartHandler(product)}>Move to cart</button>
     </section>) )}
+=======
+        {wishItems.map (product => (<div key = {product._id} className="wishlist-product-container">
+                 <img className="product-image" src= {productImg} alt="product-image" />
+ 
+                 <i className="material-icons like-icon " onClick = {() => addToWishlistHandler(product)}> favorite</i>
+ 
+                 <div className="wishlist-product-info">
+                     <p className="wishlist-info-text margin-top-bottom ">
+                         {product.name}
+                     </p>
+                     <p className="price-text margin-top-bottom "> Rs <span class = "line-through"> {product.originalPrice}</span> {product.discountedPrice}  </p> 
+                     <p className = "margin-top-bottom">Rating : {product.rating}</p>
+                 </div>
+                 <button onClick={ () => moveToCartHandler(product) } className="add-to-cart-btn primary-bg">Move to cart</button>
+             </div>) )}
+>>>>>>> Stashed changes
      
    </main>  
     </div>
