f4a9949a50f8d687d6660f214cdb9cb945d175f6
syed shaida <syedshaida295@gmail.com>
2022-04-16 11:09:27 +0530

    Added product context

--- src/contexts/product-context/product-context.js
+++ src/contexts/product-context/product-context.js
@@ -3,11 +3,8 @@ import {createContext , useContext , useState} from "react"
 const ProductContext = createContext()
 
 const ProductProvider = ({children}) => {
-  
-    const [cartItems , setCartItems] = useState([])
-    const [cartCount , setCartCount] = useState(0)
     const [productList , setProductList] = useState([])
-    return ( <ProductContext.Provider value = {{cartItems,setCartItems , productList , setProductList , cartCount, setCartCount }}>
+    return ( <ProductContext.Provider value = {{ productList , setProductList }}>
         {children}
     </ProductContext.Provider>
     )}
