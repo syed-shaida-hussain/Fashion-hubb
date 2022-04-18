64190ade2605fbd1665c521f61136991b113b575
syed shaida <syedshaida295@gmail.com>
2022-04-18 05:56:42 +0530

    Refactor : added src and alt using database

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -39,7 +39,7 @@ const Cart = () => {
 
     return  <div>
 
-        <Header />
+    <Header />
 
     {cartItems.length < 1 && <div className = "center ">
         <h2 className="margin-top-bottom">Your Shopping Cart is empty !</h2>
@@ -53,7 +53,7 @@ const Cart = () => {
 
         <main class="product-main-container">
             {cartItems.map(product => <div class="product-container">
-                <img class="product-image" src= {productImg} />
+                <img class="product-image" src= {product.src.url} alt = {product.src.alt} />
                 <div class="product-info-text">
                     <p class="info-text">{product.name}</p>
                     <p class="price-text">
@@ -71,46 +71,6 @@ const Cart = () => {
         </div>}
 
  
-=======
-import { useProduct } from "../../contexts/product-context/product-context";
-import {Header} from "../../components/Header"
-
-const Cart = () => {
-    const {cartItems , setCartItems, cartCount , setCartCount} = useProduct()
-
-    function removeFromCertHandler(product){
-        setCartItems(cartItems.filter(item => item._id !== product._id ))
-        setCartCount(prev => prev -1)
-    }
-
-    function addToWishlistHandler(product){
-      
-    }
-
-    return  <div>
-       <Header />
-
-    <h1 class="heading-text center-text">My Cart({cartCount})</h1>
-
-    <hr />
-
-    <main class="product-main-container">
-    {cartItems.map(product => <div class="product-container">
-                    <img class="product-image" src= {product.src.url} alt = {product.src.alt} />
-                    <div class="product-info-text">
-                        <p class="info-text">{product.name}</p>
-                        <p class="price-text">
-                       Rs. {product.discountedPrice} <span class="original-price">{product.originalPrice}</span>
-                      </p>
-                      <p class="product-quantity">
-                        Quantity <button class="cart-increment-btn">-</button> 1
-                        <button class="cart-decrement-btn">+</button>
-                      </p>
-                      <button class="remove-cart-btn" onClick={ () => removeFromCertHandler(product)}>Remove from cart</button>
-                      <button class="add-to-wishlist-btn" onClick = {() => addToWishlistHandler}>Add to wishlist</button>
-                    </div>
-                  </div>)}
-    </main>
     </div>
 }
 
